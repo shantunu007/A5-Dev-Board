8f060a8e4acc94b8dad0d9b9f85f7142dd1d7353
shantunu007 <chakmashantunu.web@gmail.com>
2025-03-01 10:05:41 +0600

    clear history js problem solved

--- js/complete.js
+++ js/complete.js
@@ -7,6 +7,19 @@ let increaseValue = parseInt(increaseElement.innerText);
 const counterElement = document.getElementById("counter");
 
 let counterValue = parseInt(counterElement.innerText);
+const container = document.getElementById("history-container");
+
+const titles = [
+    "Fix Mobile Button Issue",
+    "Add Dark Mode",
+    "Optimize Home Page",
+    "Add New Emoji",
+    "Integrate Open AI API",
+    "Improve Job Searching"
+
+]
+
+let titleIndex = 0;
 
 for (const button of completeButtons) {
     button.addEventListener("click", function() {
@@ -17,26 +30,33 @@ for (const button of completeButtons) {
             increaseElement.innerText = increaseValue;
             button.disabled = true;
 
-            const container = document.getElementById("history");
-            const div = document.createElement("div");
-            div.classList.add("bg-gray-400");
-            div.innerHTML = `
-                <p>You have completed the fix mobile button issue</p>
-            `
-
-            container.appendChild(div);
 
         }
-    });
-}
-
-for (const button of completeButtons) {
-    button.addEventListener("click", function() {
-        
         if (counterValue > 0) { 
             counterValue--;
             counterElement.innerText = counterValue;
             button.disabled = true;
         }
+        if(titleIndex < titles.length){
+            const div = document.createElement("div");
+            div.classList.add("bg-blue-100");
+            div.classList.add("my-3");
+            div.classList.add("px-3");
+            div.classList.add("py-6");
+            div.classList.add("rounded-xl");
+            div.classList.add("w-80");
+            div.classList.add("mx-auto");
+            div.innerHTML = `
+                <p>You have completed the ${titles[titleIndex]}</p>
+            `
+
+            container.appendChild(div);
+
+            titleIndex++;
+        }
     });
 }
+
+document.getElementById("clear-button").addEventListener("click", function(){
+    container.innerHTML = "";
+})
