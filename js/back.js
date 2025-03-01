7c36efc55e5f7a4431dce356450dd9203c248ede
shantunu007 <chakmashantunu.web@gmail.com>
2025-03-01 07:56:05 +0600

    completed the discover to blog js problem

---
+++ js/back.js
@@ -0,0 +1,4 @@
+
+document.getElementById("back-button").addEventListener("click", function(){
+    window.location.href = "./main.html"
+})
---
+++ js/blog.js
@@ -0,0 +1,7 @@
+
+document.getElementById("blog-button").addEventListener("click", function(){
+    window.location.href = "./blog.html";
+});
+
+
+
